60cdd41d4b33daca73ddb26e28930e1531ea434f
Johannes Dörr <mail@johannesdoerr.de>
2021-11-28 15:03:37 +0100

    Changed rel to remote_field

--- onetomany/models.py
+++ onetomany/models.py
@@ -12,11 +12,11 @@ class OneToManyField(models.ManyToManyField):
         #  1) There is a manually specified intermediate, or
         #  2) The class owning the m2m field is abstract.
         #  3) The class owning the m2m field has been swapped out.
-        auto_intermediate = not self.rel.through and not cls._meta.abstract and not cls._meta.swapped
+        auto_intermediate = not self.remote_field.through and not cls._meta.abstract and not cls._meta.swapped
 
         #One call super contribute_to_class and have django create the intermediate model.
         super(OneToManyField, self).contribute_to_class(cls, name, **kwargs)
 
         if auto_intermediate:
             #Set unique_together to the 'to' relationship, this ensures a OneToMany relationship.
-            self.rel.through._meta.unique_together = ((self.rel.through._meta.unique_together[0][1],),)
+            self.remote_field.through._meta.unique_together = ((self.remote_field.through._meta.unique_together[0][1],),)
